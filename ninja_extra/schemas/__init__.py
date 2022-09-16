4bffc3ca6d4b3be1aa3fad80eedf7e0eeb98bb1b
eadwinCode <ezeudoh.tochukwu@gmail.com>
2022-09-16 06:28:00 +0100

    moved object serialization and persitance functions to modelcontrollerschema class

--- ninja_extra/schemas/__init__.py
+++ ninja_extra/schemas/__init__.py
@@ -1,7 +1,13 @@
 from .response import (
+    ModelControllerSchema,
     NinjaPaginationResponseSchema,
     PaginatedResponseSchema,
     RouteParameter,
 )
 
-__all__ = ["PaginatedResponseSchema", "RouteParameter", "NinjaPaginationResponseSchema"]
+__all__ = [
+    "PaginatedResponseSchema",
+    "RouteParameter",
+    "NinjaPaginationResponseSchema",
+    "ModelControllerSchema",
+]
